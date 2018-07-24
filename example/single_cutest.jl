6e62855c1dd7de82ccecf0b2eb47abf7434955fe
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2018-07-24 15:36:41 -0300

    Back to the simple example

--- example/single_cutest.jl
+++ example/single_cutest.jl
@@ -13,8 +13,8 @@ finalize(nlp)
 solver = AlgencanSolver(epsfeas=1.0e-5, epsopt=1.0e-5,
     efstain=sqrt(1.0e-5), eostain=1.0e-5^1.5, efacc=sqrt(1.0e-5),
     eoacc=sqrt(1.0e-5),
-    ITERATIONS_OUTPUT_DETAIL=15, NUMBER_OF_ARRAYS_COMPONENTS_IN_OUTPUT=0)
-nlp = CUTEstModel("MPC7")
+    ITERATIONS_OUTPUT_DETAIL=10, NUMBER_OF_ARRAYS_COMPONENTS_IN_OUTPUT=0)
+nlp = CUTEstModel("HS6")
 model = NLPtoMPB(nlp, solver)
 MathProgBase.optimize!(model)
 println("Solver status = ", status(model))
