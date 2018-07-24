fea6d9ac8a69522ed127e4de61710fc052ae6539
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2018-07-24 15:07:03 -0300

    Fix in the build?

--- deps/build.jl
+++ deps/build.jl
@@ -9,18 +9,20 @@ libalgencan = library_dependency("libalgencan")
 udir = "algencan-3.1.1"
 algencan_dirname = joinpath(BinDeps.depsdir(libalgencan), "src", udir)
 
-provides(Sources, URI("https://www.ime.usp.br/~egbirgin/tango/algencan-3.1.1.tgz"), libalgencan, unpacked_dir=udir)
+provides(Sources, URI("http://www.ime.usp.br/~egbirgin/tango/sources/algencan-3.1.1.tgz"), libalgencan, unpacked_dir=udir)
 
 # Download
 provides(SimpleBuild,
          (@build_steps begin
             # Download and untar
             GetSources(libalgencan)
-            ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
-            CreateDirectory("src")
-            CreateDirectory("usr")
-            CreateDirectory("usr/lib")
-            `tar -zxf downloads/algencan-3.1.1-beta.tgz -C src/` # Remove this later
+            @build_steps begin
+              ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
+              CreateDirectory("src")
+              CreateDirectory("usr")
+              CreateDirectory("usr/lib")
+              `tar -zxf downloads/algencan-3.1.1-beta.tgz -C src/` # Remove this later                
+            end
             @build_steps begin
               ChangeDirectory(algencan_dirname)
               # Compile with Makefile and flags
