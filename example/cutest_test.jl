213172497f19c1341df2ee994c26de74a04f6d9d
Paulo J. S. Silva <pjssilva@gmail.com>
2018-08-01 13:51:16 -0300

    Cleaning up cute_test.jl and save the list of selected problems.

--- example/cutest_test.jl
+++ example/cutest_test.jl
@@ -50,15 +50,22 @@ end
 cutest_bench("HS6")
 
 # Grab a list of CUTEst tests
-
-# Tests that generete error in Algencan, probably it tries to compute values
-# outside the functions domains and tests that take too long (Algencan does not
-# have a timeout option).
-avoid = ["SPINOP", "DITTERT", "LEUVEN4", "KTMODEL", "TRO21X5", "NUFFIELD", "SPIN"]
 test_problems = CUTEst.select(;min_var=200, max_var=2000, min_con=10)
-# Exclude tests that are known to take too long
+
+# Aoid tests that generete error in Algencan, probably it tries to compute
+# values outside the functions domains and tests that take too long (Algencan
+# does not have a timeout option).
+avoid = ["SPINOP", "DITTERT", "LEUVEN4", "KTMODEL", "TRO21X5", "NUFFIELD", "SPIN"]
+# Exclude problematic tests
 test_problems = filter(name -> name ∉ avoid, test_problems)
 n_tests = length(test_problems)
+
+# Save the list of test_problems
+selection = open("cutest_selection.txt", "w")
+for p in test_problems
+    write(selection, string(p, "\n"))
+end
+close(selection)
 
 # Run benchmarks
 report = open(string(solver_name, "_cutest.txt"), "w")
