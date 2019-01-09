5007c8a68cbb339b53dd12f1f7d5bde88d232bba
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-09 16:55:50 -0500

    debug

--- src/Algencan.jl
+++ src/Algencan.jl
@@ -369,22 +369,15 @@ function julia_fc(n::Cint, x_ptr::Ptr{Float64}, obj_ptr::Ptr{Float64},
     unsafe_store!(obj_ptr, model.sense*obj_val)
     g = unsafe_wrap(Array, g_ptr, Int(m))
     MPB.eval_g(model.evaluator, g, x)
+    println("g = ", g[9:11])
 
     # Treat lower bounds and two-sided constraints
     if model.g_has_lb
         first_g = view(g, 1:model.m)
+        first_g .*= model.g_sense
         g[model.m + 1:m] = -first_g[model.g_two_sides] +
             model.g_lb[model.g_two_sides]
-        # TODO: I have the feeling that this is overkill and JuMP will only allow
-        # lower bound contraints when there are two bounds and hence
-        # g_sense would always be equal to 1.0
-        for i = 1:model.m
-            if model.g_sense[i] == 1.0
-                g[i] -= model.g_ub[i]
-            else
-                g[i] = -g[i] + model.g_lb[i]
-            end
-        end
+        first_g .-= model.g_ub
     else
         g .-= model.g_ub
     end
