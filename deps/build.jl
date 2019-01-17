c01fa70ca71e14586821306645a538167c59a60b
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-17 18:15:50 -0500

    Let's start over

--- deps/build.jl
+++ deps/build.jl
@@ -7,11 +7,11 @@ udir = "metis-4.0.3"
 metis_dir = joinpath(BinDeps.depsdir(libmetis), "src", udir)
 provides(Sources, URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz"), libmetis, unpacked_dir=udir)
 
-ma57_src = ENV["MA57_SOURCE"]
-libhsl_ma57 = library_dependency("libhsl_ma57")
-ma57_dir = joinpath(BinDeps.depsdir(libhsl_ma57), "src", "hsl_ma57-5.2.0")
+# ma57_src = ENV["MA57_SOURCE"]
+# libhsl_ma57 = library_dependency("libhsl_ma57")
+# ma57_dir = joinpath(BinDeps.depsdir(libhsl_ma57), "src", "hsl_ma57-5.2.0")
 
-src_dir = joinpath(BinDeps.depsdir(libhsl_ma57), "src")
+# src_dir = joinpath(BinDeps.depsdir(libhsl_ma57), "src")
 
 # metis
 provides(SimpleBuild,
@@ -20,23 +20,23 @@ provides(SimpleBuild,
   end), libmetis, os = :Linux
 )
 
-# HSL
-provides(SimpleBuild, 
-  (@build_steps begin
-      CreateDirectory(ma57_dir)
-      `tar xvf $ma57_src --directory=$src_dir`
-      @build_steps begin
-        ChangeDirectory(ma57_dir)
-        `patch -p1 <../../patches/patch_ma57.txt`
-        `./configure --prefix=$ma57_dir CFLAGS=-fPIC FCFLAGS=-fPIC `
-        `make`
-        `make install`
-      end
-      @build_steps begin
-        ChangeDirectory(joinpath(ma57_dir, "lib"))
-        `gcc --shared -o libhsl_ma57.so libhsl_ma57.a`
-      end
-  end), libhsl_ma57, os = :Linux
-)
+# # HSL
+# provides(SimpleBuild, 
+#   (@build_steps begin
+#       CreateDirectory(ma57_dir)
+#       `tar xvf $ma57_src --directory=$src_dir`
+#       @build_steps begin
+#         ChangeDirectory(ma57_dir)
+#         `patch -p1 <../../patches/patch_ma57.txt`
+#         `./configure --prefix=$ma57_dir CFLAGS=-fPIC FCFLAGS=-fPIC `
+#         `make`
+#         `make install`
+#       end
+#       @build_steps begin
+#         ChangeDirectory(joinpath(ma57_dir, "lib"))
+#         `gcc --shared -o libhsl_ma57.so libhsl_ma57.a`
+#       end
+#   end), libhsl_ma57, os = :Linux
+# )
 
-@BinDeps.install Dict(:libhsl_ma57 => :libhsl_ma57, :libmetis => :libmetis)
+@BinDeps.install Dict(:libmetis => :libmetis)
