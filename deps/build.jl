e24c76b49583429915efe3e764b7aea7f5a6102e
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-17 12:41:23 -0500

    Separate builds, pt 13

--- deps/build.jl
+++ deps/build.jl
@@ -3,10 +3,10 @@ using BinDeps
 # TODO: Allow using HSL
 @BinDeps.setup
 
-libalgencan = library_dependency("libalgencan")
-udir = "algencan-3.1.1"
-algencan_dirname = joinpath(BinDeps.depsdir(libalgencan), "src", udir)
-provides(Sources, URI("http://www.ime.usp.br/~egbirgin/tango/sources/algencan-3.1.1.tgz"), libalgencan, unpacked_dir=udir)
+# libalgencan = library_dependency("libalgencan")
+# udir = "algencan-3.1.1"
+# algencan_dirname = joinpath(BinDeps.depsdir(libalgencan), "src", udir)
+# provides(Sources, URI("http://www.ime.usp.br/~egbirgin/tango/sources/algencan-3.1.1.tgz"), libalgencan, unpacked_dir=udir)
 
 compile_hsl = "MA57_SOURCE" in keys(ENV)
 # if compile_hsl
@@ -41,53 +41,53 @@ provides(SimpleBuild,
 
 #           end), libma57, os = :Linux)
 
-# Algencan
-provides(SimpleBuild,
-         (@build_steps begin
-            # Download and untar
-            GetSources(libalgencan)
-            @build_steps begin
-              ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
-              CreateDirectory("src")
-              CreateDirectory("usr")
-              CreateDirectory("usr/lib")
-              `tar -zxf downloads/algencan-3.1.1.tgz -C src/` # Remove this later
-            end
-            @build_steps begin
-              ChangeDirectory(algencan_dirname)
-              # Compile with Makefile and flags
-              `make CFLAGS="-O3 -fPIC" FFLAGS="-O3 -ffree-form -fPIC"`
-              # Produce a shared library on deps/usr/lib
-              `gcc -shared -o ../../usr/lib/libalgencan.so
-                    -Wl,--whole-archive lib/libalgencan.a
-                    -Wl,--no-whole-archive -lgfortran`
-            end
-          end), libalgencan, os = :Linux)
+# # Algencan
+# provides(SimpleBuild,
+#          (@build_steps begin
+#             # Download and untar
+#             GetSources(libalgencan)
+#             @build_steps begin
+#               ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
+#               CreateDirectory("src")
+#               CreateDirectory("usr")
+#               CreateDirectory("usr/lib")
+#               `tar -zxf downloads/algencan-3.1.1.tgz -C src/` # Remove this later
+#             end
+#             @build_steps begin
+#               ChangeDirectory(algencan_dirname)
+#               # Compile with Makefile and flags
+#               `make CFLAGS="-O3 -fPIC" FFLAGS="-O3 -ffree-form -fPIC"`
+#               # Produce a shared library on deps/usr/lib
+#               `gcc -shared -o ../../usr/lib/libalgencan.so
+#                     -Wl,--whole-archive lib/libalgencan.a
+#                     -Wl,--no-whole-archive -lgfortran`
+#             end
+#           end), libalgencan, os = :Linux)
 
-# TODO: see if it is possible to merge most of this two recipes.
-# This is mostly a dirty trick to get it compiling in OS X
-# as it does not accept --whole-archive in ld
-provides(SimpleBuild,
-         (@build_steps begin
-            # Download and untar
-            GetSources(libalgencan)
-            @build_steps begin
-              ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
-              CreateDirectory("src")
-              CreateDirectory("usr")
-              CreateDirectory("usr/lib")
-              `tar -zxf downloads/algencan-3.1.1.tgz -C src/` # Remove this later
-            end
-            @build_steps begin
-              ChangeDirectory(algencan_dirname)
-              # Compile with Makefile and flags
-              `make CFLAGS="-O3 -fPIC" FFLAGS="-O3 -ffree-form -fPIC"`
-              # Produce a shared library on deps/usr/lib
-              `gfortran -shared -o ../../usr/lib/libalgencan.dylib
-                    -Wl,-all_load lib/libalgencan.a
-                    -Wl,-noall_load -lgfortran`
-            end
-          end), libalgencan, os = :Darwin)
+# # TODO: see if it is possible to merge most of this two recipes.
+# # This is mostly a dirty trick to get it compiling in OS X
+# # as it does not accept --whole-archive in ld
+# provides(SimpleBuild,
+#          (@build_steps begin
+#             # Download and untar
+#             GetSources(libalgencan)
+#             @build_steps begin
+#               ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
+#               CreateDirectory("src")
+#               CreateDirectory("usr")
+#               CreateDirectory("usr/lib")
+#               `tar -zxf downloads/algencan-3.1.1.tgz -C src/` # Remove this later
+#             end
+#             @build_steps begin
+#               ChangeDirectory(algencan_dirname)
+#               # Compile with Makefile and flags
+#               `make CFLAGS="-O3 -fPIC" FFLAGS="-O3 -ffree-form -fPIC"`
+#               # Produce a shared library on deps/usr/lib
+#               `gfortran -shared -o ../../usr/lib/libalgencan.dylib
+#                     -Wl,-all_load lib/libalgencan.a
+#                     -Wl,-noall_load -lgfortran`
+#             end
+#           end), libalgencan, os = :Darwin)
 
 # @BinDeps.install Dict(:libalgencan => :libalgencan)
 @BinDeps.install Dict(:libmetis => :libmetis)
