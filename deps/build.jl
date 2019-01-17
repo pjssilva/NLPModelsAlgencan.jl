84084456df6428ec56e94662f8715abbbbf0d59f
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-17 13:22:14 -0500

    Separate builds, pt 27

--- deps/build.jl
+++ deps/build.jl
@@ -3,17 +3,17 @@ using BinDeps
 # TODO: Allow using HSL
 @BinDeps.setup
 
+# libalgencan = library_dependency("libalgencan")
+# udir = "algencan-3.1.1"
+# algencan_dirname = joinpath(BinDeps.depsdir(libalgencan), "src", udir)
+# provides(Sources, URI("http://www.ime.usp.br/~egbirgin/tango/sources/algencan-3.1.1.tgz"), libalgencan, unpacked_dir=udir)
+
+# compile_hsl = "MA57_SOURCE" in keys(ENV)
+# if compile_hsl
 libmetis = library_dependency("libmetis")
 udir = "metis-4.0.3"
 metis_dirname = joinpath(BinDeps.depsdir(libmetis), "src", udir)
 provides(Sources, URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz"), libmetis, unpacked_dir=udir)
-
-# compile_hsl = "MA57_SOURCE" in keys(ENV)
-# if compile_hsl
-# libmetis = library_dependency("libmetis")
-# udir = "metis-4.0.3"
-# metis_dirname = joinpath(BinDeps.depsdir(libmetis), "src", udir)
-# provides(Sources, URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz"), libmetis, unpacked_dir=udir)
   
   # libma57 = library_dependency("libhsl_ma57")
   # maudir = "hsl_ma57-5.2.0"
@@ -21,10 +21,10 @@ provides(Sources, URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis
 # end
 
 # Metis
-# provides(SimpleBuild,
-#          (@build_steps begin
-#             # Download and untar
-#             GetSources(libmetis)
+provides(SimpleBuild,
+         (@build_steps begin
+            # Download and untar
+            GetSources(libmetis)
 #             @build_steps begin
 #               ChangeDirectory(BinDeps.depsdir(libmetis))        # Possibly remove
 #               CreateDirectory("src")
@@ -41,7 +41,7 @@ provides(Sources, URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis
 #                     -Wl,--whole-archive lib/libalgencan.a
 #                     -Wl,--no-whole-archive -lgfortran`
 #             end
-          # end), libmetis, os = :Linux)
+          end), libmetis, os = :Linux)
 
 # provides(SimpleBuild,
 #          (@build_steps begin
@@ -73,25 +73,24 @@ provides(Sources, URI("http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis
 provides(SimpleBuild,
          (@build_steps begin
             # Download and untar
-            GetSources(libmetis)
+            GetSources(libalgencan)
             @build_steps begin
-              ChangeDirectory(BinDeps.depsdir(libmetis))        # Possibly remove
+              ChangeDirectory(BinDeps.depsdir(libalgencan))        # Possibly remove
               CreateDirectory("src")
               CreateDirectory("usr")
               CreateDirectory("usr/lib")
-              `tar -zxf downloads/metis-4.0.3.tar.gz -C src/` # Remove this later
+              `tar -zxf downloads/algencan-3.1.1.tgz -C src/` # Remove this later
             end
             @build_steps begin
-              ChangeDirectory(metis_dirname)
+              ChangeDirectory(algencan_dirname)
               # Compile with Makefile and flags
-              `make COPTIONS="-fPIC"`
-              # `make CFLAGS="-O3 -fPIC" FFLAGS="-O3 -ffree-form -fPIC"`
-              # # Produce a shared library on deps/usr/lib
-              # `gcc -shared -o ../../usr/lib/libalgencan.so
-              #       -Wl,--whole-archive lib/libalgencan.a
-              #       -Wl,--no-whole-archive -lgfortran`
+              `make CFLAGS="-O3 -fPIC" FFLAGS="-O3 -ffree-form -fPIC"`
+              # Produce a shared library on deps/usr/lib
+              `gcc -shared -o ../../usr/lib/libalgencan.so
+                    -Wl,--whole-archive lib/libalgencan.a
+                    -Wl,--no-whole-archive -lgfortran`
             end
-          end), libmetis, os = :Linux)
+          end), libalgencan, os = :Linux)
 
 # # Algencan
 # provides(SimpleBuild,
