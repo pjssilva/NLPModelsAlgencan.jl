933502e3a075f1c4863c2ffa730058dfd6218917
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-17 13:43:35 -0500

    Separate builds, pt 35

--- deps/build.jl
+++ deps/build.jl
@@ -72,7 +72,7 @@ provides(SimpleBuild,
             @build_steps begin
               ChangeDirectory(ma57_dirname)
               `patch -p1 <../../patches/patch_ma57.txt`
-              `./configure --prefix=$ma57_dirname CFLAGS=-fPIC FCFLAGS=-fPIC`
+              `configure --prefix="$ma57_dirname" CFLAGS="-fPIC" FCFLAGS=-"fPIC"`
               `make`
             end
           end), libhsl_ma57, os = :Linux)
