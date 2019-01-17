2a52cb5b35e75fcfdc62e4643cd7c773ec3b3c3d
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-17 17:32:46 -0500

    Let's start over, 3

--- deps/build.jl
+++ deps/build.jl
@@ -10,7 +10,7 @@ ma57_dir = joinpath(BinDeps.depsdir(libhsl_ma57), "src", "hsl_ma57-5.2.0")
 provides(SimpleBuild, 
   (@build_steps begin
       CreateDirectory(ma57_dir)
-      FileUnpacker(ENV["MA57_SOURCE"], src_dir, ma57_dir)
+      FileUnpacker(ENV["MA57_SOURCE"], src_dir, "")
   end), libhsl_ma57, os = :Linux)
 
-@BinDeps.install Dict(:libhsl_ma57 => :libhsl_ma57)
+# @BinDeps.install Dict(:libhsl_ma57 => :libhsl_ma57)
