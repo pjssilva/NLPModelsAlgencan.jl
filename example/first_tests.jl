0ade988197d78b1c93bcee2ee046808334c073f1
Paulo J. S. Silva <pjssilva@ime.unicamp.br>
2019-01-22 18:12:35 -0500

    We don't need to depend on Ipopt.jl

--- example/first_tests.jl
+++ example/first_tests.jl
@@ -1,12 +1,10 @@
 using JuMP
-using Ipopt
 using Algencan
 import MathProgBase
 MPB = MathProgBase
 
 println("Building the first model...\n")
 m = Model(solver=AlgencanSolver(epsopt=1.0e-5, epsfeas=1.0e-5))
-# m = Model(solver=IpoptSolver())
 @variable(m, 3.0 <= var[1:2] <= 5.0)
 @NLobjective(m, :Min, 123.45*(var[1] - π)^2 + sin(var[1]) + (var[2] - ℯ)^2 + sin(π - var[2]))
 println(m)
@@ -19,7 +17,6 @@ println(getvalue(var), "\n\n")
 
 println("Building second model...\n")
 m2 = Model(solver=Algencan.AlgencanSolver(epsfeas=1.0e-8, epsopt=1.0e-5))
-# m2 = Model(solver=IpoptSolver())
 @variable(m2, 2.0 <= var2[1:3] <= 5.0)
 @NLobjective(m2, :Min, (var2[1] - 4)^2 + (var2[2] - 4)^2 + var2[3])
 @constraint(m2, -5 <= -(var2[1] + var2[2]))
