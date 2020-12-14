52b8f1453f1b77e7e4754723f00c0da62f361f13
Ilan Francisco da Silva <i257199@dac.unicamp.br>
2020-12-14 17:07:59 -0300

    Basic model test NLPModels compatible

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,18 @@
-using Algencan, JuMP, Test
+using NLPModels, NLPModelsAlgencan, Test
 
 @testset "Basic model test" begin
-  model = Model(solver = AlgencanSolver())
-  @variable(model, x[1:2])
-  @objective(model, Min, 4 * x[1]^2 + 9 * x[2]^2)
-  @constraint(model, c, x[1] + x[2] == 1.0)
+  f(x) = 4 * x[1]^2 + 9 * x[2]^2
+  x0   = zeros(2)
+  c(x) = [x[1] + x[2]]
+  lcon = [1.0]
+  ucon = [1.0]
 
-  status = solve(model)
-  @test status == :Optimal
+  nlp = ADNLPModel(f, x0, c, lcon, ucon)
 
-  x = getvalue(x)
-  @test x ≈ [9 / 13; 4 / 13]
+  output = algencan(nlp)
+  @test output.status == :first_order
 
-  λ = getdual(c)
-  @test λ ≈ -72 / 13
+  @test output.solution ≈ [9 / 13; 4 / 13]
+
+  @test output.multipliers ≈ [-72 / 13]
 end
