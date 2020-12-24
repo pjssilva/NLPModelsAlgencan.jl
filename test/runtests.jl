d18ba96b9f9fb4c2a08ce9081d03b98f0c17af15
Ilan Francisco da Silva <i257199@dac.unicamp.br>
2020-12-24 14:32:21 -0300

    add autodiff model tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,34 @@
-using NLPModels, NLPModelsAlgencan, Test
+using NLPModels, NLPModelsAlgencan, Test, LinearAlgebra, CUTEst, NLPModelsIpopt
 
-@testset "Basic model test" begin
-  f(x) = 4 * x[1]^2 + 9 * x[2]^2
-  x0   = zeros(2)
-  c(x) = [x[1] + x[2]]
-  lcon = [1.0]
-  ucon = [1.0]
+@testset "Autodiff tests" begin
+  include("problems/autodiff/hs12.jl")
+  include("problems/autodiff/hs52.jl")
+  include("problems/autodiff/hs104.jl")
 
-  nlp = ADNLPModel(f, x0, c, lcon, ucon)
+  stats = algencan(hs12())
+  @test stats.status     == :first_order
+  @test stats.objective   ≈ -30.0 rtol=1.0e-6
+  @test stats.solution    ≈ [2.0, 3.0] rtol=1.0e-6
+  @test stats.multipliers ≈ [-0.5] rtol=1.0e-6
 
-  output = algencan(nlp)
-  @test output.status == :first_order
+  stats = algencan(hs52())
+  @test stats.status     == :first_order
+  @test stats.objective   ≈ 1859/349 rtol=1.0e-6
+  @test stats.solution    ≈ [-0.0945558739311759, 0.03151862464668532,
+                            0.5157593121810534, -0.4527220628824326,
+                            0.031518624649457136] rtol=1.0e-6
+  @test stats.multipliers ≈ [3.277936962780388, 2.905444126051696,
+                             -7.747851002665478] rtol=1.0e-6
 
-  @test output.solution ≈ [9 / 13; 4 / 13]
+  stats = algencan(hs104())
+  @test stats.status     == :first_order
+  @test stats.objective   ≈ 3.951163439 rtol=1.0e-6
+  @test stats.solution    ≈ [6.465114030593916, 2.232708645245993,
+                             0.6673974911544844, 0.5957564229231442,
+                             5.932675677556293, 5.527234565979492,
+                             1.01332200852064, 0.4006682289818261] rtol=1.0e-6
+  @test stats.multipliers ≈ [-2.3596898286423866, -6.205501994097753,
+                             -0.9276198475360146, -0.8472008503830812,
+                              0.0] rtol=1.0e-6
 
-  @test output.multipliers ≈ [-72 / 13]
 end
